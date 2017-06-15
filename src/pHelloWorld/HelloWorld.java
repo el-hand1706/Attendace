b2f4267c697aa04e3bb670cff7578916cc2dfb7f
iyoda tomoyuki <iyoda@el-hand.co.jp>
2017-06-15 10:38:34 +0900

    初期　及び　HelloWorld

---
+++ src/pHelloWorld/HelloWorld.java
@@ -0,0 +1,54 @@
+package pHelloWorld;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorld() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	    // TODO Auto-generated method stub
+	    PrintWriter out = response.getWriter();
+	    out.println("<html>");
+	    out.println("<head>");
+	    out.println("<title>Hello World!</title>");
+	    out.println("</head>");
+	    out.println("<body>");
+	    out.println("<h1>Hello World!</h1>");
+	    out.println("<p>First Servlet</p>");
+	    out.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
